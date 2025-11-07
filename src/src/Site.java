6811607277c200d9dc58b3ffc12f254fcc99c0a1
Noor ElShanawany <noorelshanawany@cmail.carleton.ca>
2025-11-07 09:17:46 -0500

    Commit 3: Made Site abstract and added Template Method

--- src/src/Site.java
+++ src/src/Site.java
@@ -1,4 +1,4 @@
-public class Site {
+public abstract class Site {
     protected double units;
     protected double rate;
 
@@ -7,13 +7,21 @@ public class Site {
         this.rate = rate;
     }
 
-    public double getBillableAmount() {
+    // Template Method: defines the full algorithm
+    public final double getBillableAmount() {
         double base = getBaseAmount();
-        double tax = base * 0.05;
-        return base + tax;
+        double tax = getTax(base);
+        return base + tax + getTypeSpecificCharge(base);
     }
 
-    private double getBaseAmount() {
+    protected double getBaseAmount() {
         return units * rate;
     }
+
+    protected double getTax(double base) {
+        return base * 0.05;
+    }
+
+    // Hook: implemented differently by subclasses
+    protected abstract double getTypeSpecificCharge(double base);
 }
